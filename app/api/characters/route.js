d7e125d9531ebfcd4a4ebf519b5a2e7d53a31180
Ashlyn Knox <ashx3s@tuta.io>
2025-12-04 15:53:55 -0700

    characters crud operations

--- app/api/characters/route.js
+++ app/api/characters/route.js
@@ -6,47 +6,67 @@ export async function GET() {
 
 export async function POST(request) {
   const body = await request.json();
+
+  // extract information from the body
   const { name, species, description } = body;
+
+  // send error codes for required fields
   if (!name || !species) {
     return Response.json(
-      { error: "Name and species are required fields" },
+      {
+        error: "Name and species are required fields",
+      },
       { status: 400 }
     );
   }
+
+  // create a new character
   const newCharacter = {
     id:
-      characters.length > 0 ? Math.max(...characters.map((c) => c.id)) + 1 : 1,
+      characters.length > 0
+        ? Math.max(...characters.map((character) => character.id)) + 1
+        : 1,
     name,
     species,
     description: description || "",
   };
+  // add the character to the data
   characters.push(newCharacter);
+  //  return our new character as a response.json
   return Response.json(newCharacter, { status: 201 });
 }
 
 export async function DELETE(request) {
+  // get a body from the request
   const body = await request.json();
+  // extract the id of what's to be delete
   const { id } = body;
+  // if there's no id handle error with 400
   if (!id) {
     return Response.json(
       { error: "ID is required to delete a character" },
       { status: 400 }
     );
   }
-  const index = characters.findIndex((char) => char.id === id);
+  // use findIndex to identify the character based off the id
+  const index = characters.findIndex((character) => character.id === id);
   if (index === -1) {
     return Response.json(
       { error: `Character with id ${id} is not found` },
       { status: 404 }
     );
   }
+  // splice out the character
   const deletedCharacter = characters.splice(index, 1)[0];
+  // return response that shows deleted character
   return Response.json(
     {
       message: "Character successfully deleted",
       character: deletedCharacter,
     },
-    { status: 200 }
+    {
+      status: 200,
+    }
   );
 }
 
--- app/api/hello-world/route.js
+++
@@ -1,7 +0,0 @@
-// naming convention matters
-export function GET() {
-  return Response.json({
-    message: "Hello World",
-    uses: "Serve data on the backend",
-  });
-}
